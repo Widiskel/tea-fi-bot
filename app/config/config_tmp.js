eb4bb5d85621c31b78e7051f6f3d0a554b1171c1
widiskel <waputra16@gmail.com>
2025-02-07 23:35:29 +0700

    change code

---
+++ app/config/config_tmp.js
@@ -0,0 +1,6 @@
+export class Config {
+  static INITIALDEPOSITAMOUNT = "0.1"; //POL > WPOL
+  static TXAMOUNT = "0.001"; //WPOL > TPOL
+  static WRAPPINGCOUNT = 30;
+  static WAITFORBLOCKCONFIRMATION = true;
+}
